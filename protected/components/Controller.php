5cb89454238fd9a220b3f8bf50f1ee13b099e7e5
Lucifer.Mac pro <81434146@qq.com>
2016-05-18 14:50:14 +0800

    添加跨域头Access-Control-Allow-Origin:*

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,11 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+
+    public function beforeAction($action)
+    {
+
+        header("Access-Control-Allow-Origin:*");
+        return parent::beforeAction($action);
+    }
 }
